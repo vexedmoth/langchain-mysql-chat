0fd847fdda292594b0d680011a46449ca2ba1457
vexedmoth <vexedmoth@protonmail.com>
2024-07-31 17:51:07 +0200

    update

--- source/main.py
+++ source/main.py
@@ -9,32 +9,45 @@ from langchain_core.runnables import RunnablePassthrough
 sqlite_uri = "sqlite:///./databases/Chinook_sample.db"
 db = SQLDatabase.from_uri(sqlite_uri)
 
-# Plantilla del prompt
-template = """Based on the table schema below, write a SQL query that would answer the user's question:
-{schema}
-
-Question: {question}
-SQL Query:"""
-
-prompt = ChatPromptTemplate.from_template(template)
+llm = ChatOllama(model="llama3:latest", temperature=0)
 
 
-def get_schema(_):
-    schema = db.get_table_info()
-    return schema
+def get_sql_chain(db):
+
+    # Plantilla del prompt
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, write a SQL query that would answer the user's question:
+    {schema}
+
+    Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+    
+    
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
+    
+    Your turn:
+    
+    Question: {question}
+    SQL Query:
+    """
+
+    # Generamos el prompt
+    prompt = ChatPromptTemplate.from_template(template)
+
+    def get_schema(_):
+        return db.get_table_info()
+
+    return (
+        RunnablePassthrough.assign(schema=get_schema)
+        | prompt
+        | llm.bind(stop=["\nSQLResult:"])
+        | StrOutputParser()
+    )
 
 
-llm = ChatOllama(model="gemma:2b", temperature=0)
-
-sql_chain = (
-    RunnablePassthrough.assign(schema=get_schema)
-    | prompt
-    | llm.bind(stop=["\nSQLResult:"])
-    | StrOutputParser()
-)
-
-
-user_question = "how many albums are there in the database?"
-sql_chain.invoke({"question": user_question})
-
-# 'SELECT COUNT(*) AS TotalAlbums\nFROM Album;'
+user_question = "how many artists are there in the database?"
+get_sql_chain().invoke({"question": user_question})
