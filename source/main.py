02b0a3a07122f73e787cea2d422ee593f9e2abe2
vexedmoth <vexedmoth@protonmail.com>
2024-07-31 19:08:54 +0200

    update

--- source/main.py
+++ source/main.py
@@ -9,10 +9,15 @@ from langchain_core.runnables import RunnablePassthrough
 sqlite_uri = "sqlite:///./databases/Chinook_sample.db"
 db = SQLDatabase.from_uri(sqlite_uri)
 
+# Cargamos el LLM
 llm = ChatOllama(model="llama3:latest", temperature=0)
 
 
-def get_sql_chain(db):
+def get_schema(_):
+    return db.get_table_info()
+
+
+def get_sql_chain():
 
     # Plantilla del prompt
     template = """
@@ -38,9 +43,6 @@ def get_sql_chain(db):
     # Generamos el prompt
     prompt = ChatPromptTemplate.from_template(template)
 
-    def get_schema(_):
-        return db.get_table_info()
-
     return (
         RunnablePassthrough.assign(schema=get_schema)
         | prompt
@@ -49,5 +51,37 @@ def get_sql_chain(db):
     )
 
 
+def get_response(user_question):
+    sql_chain = get_sql_chain()
+
+    # Plantilla del prompt
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, question, sql query, and sql response, write a natural language response.
+    {schema}
+
+    User question: {question}
+    SQL Query: {query}
+    SQL Response: {response}
+    """
+
+    # Generamos el prompt
+    prompt = ChatPromptTemplate.from_template(template)
+
+    full_chain = (
+        RunnablePassthrough.assign(query=sql_chain).assign(
+            schema=get_schema, response=lambda vars: db.run(vars["query"])
+        )
+        | prompt
+        | llm
+        | StrOutputParser
+    )
+
+    return full_chain.invoke({"question": user_question})
+
+
 user_question = "how many artists are there in the database?"
-get_sql_chain().invoke({"question": user_question})
+# get_sql_chain().invoke({"question": user_question})
+
+response = get_response(user_question)
+print(response)
